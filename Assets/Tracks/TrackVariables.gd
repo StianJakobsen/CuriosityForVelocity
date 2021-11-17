b048a19e319b50a8fe6d822d4a5b84902d659554
StianJakobsen <49446788+StianJakobsen@users.noreply.github.com>
2021-11-17 23:02:22 +0900

    Interface is good enough and game stops after a given number of laps has been completed

--- Assets/Tracks/TrackVariables.gd
+++ Assets/Tracks/TrackVariables.gd
@@ -2,6 +2,8 @@ extends Node
 
 
 var lap = 1
+var num_laps = 1
+
 var sector1 = false
 var sector2 = false
 var start = true
@@ -9,3 +11,4 @@ var start = true
 var last_lap_time
 
 var time_start = 0
+
