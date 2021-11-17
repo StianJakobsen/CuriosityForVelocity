7aaf5035b430288b69c809494ae2497885d06f05
Armon <armonah2@gmail.com>
2021-11-17 22:08:18 +0900

    menu partially implemented

---
+++ Script/settingsButton.gd
@@ -0,0 +1,5 @@
+extends Button
+
+func _on_settingsButton_pressed():
+	get_tree().change_scene("res://Assets/Scenes/settingsScreen.tscn")
+	pass # Replace with function body.
---
+++ Script/startButton.gd
@@ -0,0 +1,5 @@
+extends Button
+
+func _on_startButton_pressed():
+	get_tree().change_scene("res://Assets/Scenes/selectScreen.tscn")
+	pass # Replace with function body.
