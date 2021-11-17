dfabfe3e93d46b3a272f2739ca13d69c0137b3c2
StianJakobsen <49446788+StianJakobsen@users.noreply.github.com>
2021-11-17 16:43:10 +0900

    Added sectors

--- Assets/Scenes/GoalLine.gd
+++ Assets/Scenes/GoalLine.gd
@@ -1,10 +1,30 @@
 extends Area
 
-var laps = 0
-
+var lap = 1
+var sector1 = false
+var sector2 = false
 
 
 func _on_GoalLine_body_entered(body):
 	if body.is_in_group('RaceCar'):
-		laps += 1
-		print('Driven')
+		if sector1 and sector2:
+			lap += 1
+			sector1 = false
+			sector2 = false
+			print('You are on lap ' + lap)
+		else:
+			print('You have to cross sector 1 and 2')
+		
+
+
+func _on_Sector1_body_entered(body):
+	if body.is_in_group('RaceCar'):
+		sector1 = true
+		print('Sector 1 is entered')
+
+
+func _on_Sector2_body_entered(body):
+	if body.is_in_group('RaceCar'):
+		sector2 = true
+		print('Sector 2 is eneterd')
+		print(sector1)
